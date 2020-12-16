c57305db07cc162aa02484d7facec0c335cbc841
Ashleigh Wilson <ashleigh.wilson85@icloud.com>
2020-12-16 10:57:23 +0000

    Improved gaps between windows. Now more consistant between horizontal and vertical gaps.

--- dwm.c
+++ dwm.c
@@ -54,7 +54,7 @@
 #define LENGTH(X)               (sizeof X / sizeof X[0])
 #define MOUSEMASK               (BUTTONMASK|PointerMotionMask)
 #define WIDTH(X)                ((X)->w + 2 * (X)->bw + gappx)
-#define HEIGHT(X)               ((X)->h + 2 * (X)->bw + gappx)
+#define HEIGHT(X)               ((X)->h + 2 * (X)->bw + gappx * 2)
 #define TAGMASK                 ((1 << LENGTH(tags)) - 1)
 #define TEXTW(X)                (drw_fontset_getwidth(drw, (X)) + lrpad)
 
