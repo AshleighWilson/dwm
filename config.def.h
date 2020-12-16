ed1ce12325fb138e7a7bf0f9694579c76ef67a1b
Ashleigh Wilson <ashleigh.wilson85@icloud.com>
2020-12-16 12:48:13 +0000

    Small visual changes.

--- config.def.h
+++ config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int gappx     = 4;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -15,10 +15,18 @@ static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
 static const char col_white[]       = "#ffffff";
 static const char col_dark_gray[]   = "#666666";
+
+static const char color1[]          = "#ffffff";
+static const char color2[]          = "#444444";
+static const char color3[]          = "#666666";
+static const char color4[]          = "#ffff00";
+static const char color5[]          = "#282c34";
+static const char color6[]          = "#ff00ff";
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_dark_gray },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_white  },
+	[SchemeNorm] = { color1, color3, color3 },
+	[SchemeSel]  = { color1, color2, color1 },
 };
 
 /* tagging */
@@ -35,7 +43,7 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.5; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.5;  /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
